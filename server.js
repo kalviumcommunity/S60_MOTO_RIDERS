dc81d0458253b4ccfbc63e9002316418116f782e
Shiva Varma <shivavarma@Shivas-MacBook-Air.local>
2024-04-10 12:42:53 +0530

    updated code

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const mongoose = require('mongoose');
 const app = express()
 
 
-mongoose.connect('mongodb+srv://shivavarmak:shiva123@moto-riders.guadwwt.mongodb.net/?retryWrites=true&w=majority&appName=Moto-Riders')
-
-.then(
-    ()=> console.log('DB Connected')
-).catch(err => console.log(err))
-
 app.get('/ping', (req, res) => {
     res.send('pong')
 })
